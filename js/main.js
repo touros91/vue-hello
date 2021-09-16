606bb5f29341f3dc25da00cca7ebab70099553ce
touros91 <andreageraci91@gmail.com>
2021-09-16 14:57:21 +0200

    Aggiunto esercizio completo

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            message: "Benvenuto Vue.js!!!",
+            img: "img/vue.jpeg",
+            altText: "Immagine di Vue.js",
+        }
+    }
+);
